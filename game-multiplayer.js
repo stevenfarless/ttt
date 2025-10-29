e20613b0801cdfe9ca00c2dc52d7cf0fc65a75e9
Steven <125781365+stevenfarless@users.noreply.github.com>
2025-10-29 01:53:05 -0500

    Update game-multiplayer.js

--- game-multiplayer.js
+++ game-multiplayer.js
@@ -68,7 +68,27 @@ function recreatePeerConnection() {
         debug: 2,
         config: {
             iceServers: [
-                { urls: 'stun:stun.l.google.com:19302' }
+                { 
+                    urls: 'stun:stun.l.google.com:19302' 
+                },
+                {
+                    urls: 'stun:stun1.l.google.com:19302'
+                },
+                {
+                    urls: 'turn:openrelay.metered.ca:80',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject'
+                },
+                {
+                    urls: 'turn:openrelay.metered.ca:443',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject'
+                },
+                {
+                    urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject'
+                }
             ]
         }
     };
